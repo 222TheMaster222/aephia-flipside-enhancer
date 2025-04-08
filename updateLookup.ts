00c767950efdda2b9b50b81fbd5866f56953dfc1
222TheMaster222 <222themaster222@proton.me>
2025-04-08 09:55:00 -0500

    pull in fleets and player profile names

--- updateLookup.ts
+++ updateLookup.ts
@@ -4,9 +4,8 @@ import fs from 'fs';
 import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from "@solana/web3.js";
 import { AnchorProvider, Program, Wallet } from "@staratlas/anchor";
 import { byteArrayToString, readAllFromRPC } from "@staratlas/data-source";
-import { PLAYER_PROFILE_IDL, PlayerProfileIDLProgram } from "@staratlas/player-profile";
+import { PLAYER_PROFILE_IDL, PlayerName, PlayerProfile, PlayerProfileIDLProgram } from "@staratlas/player-profile";
 import { Fleet, SAGE_IDL, SageIDLProgram } from "@staratlas/sage";
-import { Sage } from "@staratlas/sage/dist/src/idl/sage";
 
 const SAGE_PROGRAM_ID = new PublicKey('SAGE2HAwep459SNq61LHvjxPk4pLPEJLoMETef7f7EE');
 const PLAYER_PROFILE_ID = new PublicKey('pprofELXjL5Kck7Jn5hCpwAL82DpTkSYBENzahVtbc9');
@@ -43,43 +42,31 @@ async function fetchLookupData() {
         AnchorProvider.defaultOptions(),
     );
 
-    const sage = new Program(SAGE_IDL, SAGE_PROGRAM_ID, provider);
-    const playerProfile = new Program(PLAYER_PROFILE_IDL, PLAYER_PROFILE_ID, provider);
+    const sageProgram = new Program(SAGE_IDL, SAGE_PROGRAM_ID, provider);
+    const playerProfileProgram = new Program(PLAYER_PROFILE_IDL, PLAYER_PROFILE_ID, provider);
 
+    const [playerProfiles, playerNames, fleets] = await Promise.all([
+        getPlayerProfiles(connection, playerProfileProgram),
+        getPlayerNames(connection, playerProfileProgram),
+        getFleets(connection, sageProgram),
+    ])
 
-    const fleets = await getFleets(connection, sage);
+    const playerNameMapObject = playerNames.reduce((acc, x) => {
+        acc[x.data.profile.toBase58()] = x.name;
+        return acc;
+    }, {});
 
-    const data: Record<string, string> = fleets.reduce((acc, f) => {
+    let data = playerProfiles.reduce((acc, x) => {
+        acc[x.key.toBase58()] = playerNameMapObject[x.key.toBase58()];
+        return acc;
+    }, {} as Record<string, string>)
+
+    data = fleets.reduce((acc, f) => {
         acc[f.key.toBase58()] = byteArrayToString(f.data.fleetLabel);
         return acc;
-      }, {} as Record<string, string>);
+    }, data);
 
-      return data;
-
-    //
-    // This dummy implementation returns hardcoded lookup data:
-    return {
-        "DRckeF77miyiTXTJzRJxP68XqoUK9Pb3j2SWrpsHC6cC": "Alice",
-        "FXtyF3qvDDGq4NrTW4AMudw7s9X3kRUxoTTYfsA5D6HB": "Bob",
-        "BVUUhbHXTwKA8YbZBRixiG3jYm1QDKPZKnJGL8F5Zb1W": "Charlie",
-        "7Ac12FJEM95X21QCFo3jC6wds1cYQxxwJHUvtEVJf3SX": "Dave",
-        "BxYDuNR9AufjcT5DWrUFQgToF77ceESSACG8EbX8DE8r": "Eve",
-        "BdNQKJw17jawL4qsKcydLHMMpUk7szcbf7oPAdF5sbFN": "Frank",
-        "4vgQD1xCZ1pZ9R8U27d2kRgJZFmeV1qc8N7ehDHbKSQd": "Grace",
-        "DCNuSEAgHMus5FGykLTPJuoraSvdNbTsUod5w6JAXw5U": "Heidi",
-        "ARgWiVzPsdTFr5xUus8DpCkK63BsS5xAc54StfFTRvu": "Ivan",
-        "FGymLRyvMYVEA1eV2FcB7v7e3rcNN1XNE34MNhrSCw61": "Judy",
-        "7X2qXTB4LR7r7s9NLp2ZZFpvQNhrmFZJ7WpyaqbkRKjx": "Karl",
-        "7e1gmus9ZU22k6inn8VoBKwEQpL3W6wZCtQhLhK6X6KN": "Leo",
-        "7jKp5e9jgjHn6mLaoHNpzze4ZC31uixfJABsj85SjC8m": "Mia",
-        "HFGnp4YDBcCUVaXQNadg42XUf4zC7vQEYoACEXnvnRmn": "Nina",
-        "8GL7D7yQi6fRpMs6dugVmz9TLSrpwYoR71ewpFiekKBL": "Oscar",
-        "HeoRu2gu4XhVHbg4BVdMX7S7XXhEDRaPUSSEC1coGrtg": "Pam",
-        "GbYJXyruJ7CqTtXr8gwZZji3dNabCq83jLk7J7ew6n4S": "Quinn",
-        "HUmgEebQnh7CDVByo4Vz85JUHXw4ashNcjGmfe3tPzmm": "Rita",
-        "En5WtGVaQZ4dcLtycMVujs6L3cYmnJXVST1sAx2TvNrz": "Steve",
-        "HDNahLDLjwT3V2JKEYWhAyRRwVnz2hemuucv4Vnw6DLh": "Trudy"
-    };
+    return data;
 }
 
 async function updateLookupFile() {
@@ -99,10 +86,7 @@ updateLookupFile().finally(() => {
     console.log('end script')
 });
 
-
-
-
-async function getFleets(connection: Connection, program: Program<Sage>): Promise<Fleet[]> {
+async function getFleets(connection: Connection, program: SageIDLProgram): Promise<Fleet[]> {
 
     return (await readAllFromRPC(
         connection,
@@ -111,4 +95,26 @@ async function getFleets(connection: Connection, program: Program<Sage>): Promis
         'confirmed'))
         .filter(p => p.type === 'ok')
         .map(p => (p as any).data as Fleet);
+}
+
+async function getPlayerProfiles(connection: Connection, program: PlayerProfileIDLProgram): Promise<PlayerProfile[]> {
+
+    return (await readAllFromRPC(
+        connection,
+        program,
+        PlayerProfile,
+        'confirmed'))
+        .filter(p => p.type === 'ok')
+        .map(p => (p as any).data as PlayerProfile);
+}
+
+async function getPlayerNames(connection: Connection, program: PlayerProfileIDLProgram): Promise<PlayerName[]> {
+
+    return (await readAllFromRPC(
+        connection,
+        program,
+        PlayerName,
+        'confirmed'))
+        .filter(p => p.type === 'ok')
+        .map(p => (p as any).data as PlayerName);
 }
